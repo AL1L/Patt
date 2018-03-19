660558ccc8a1b1c9c5dd7839017c28c34e62d0d3
AL_1 <allenmlantz@gmail.com>
2018-03-18 22:17:28 -0500

    Removed unessisary stuff

--- intents/GuildCount/intent.py
+++ intents/GuildCount/intent.py
@@ -1,12 +1,6 @@
 import aiohttp
 import discord
 import utils as u
-import os
-import sys
-from subprocess import check_output
-
-
-allowed_users = ['366085504971571200', '202150484058832905']
 
 class Intent(u.Intent):
     
