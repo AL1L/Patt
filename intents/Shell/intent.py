df296c175b57579efc1d0e9d71747805cc04c4b7
Allen Lantz <allen.m.lantz@gmail.com>
2018-03-20 11:53:44 -0500

    Shell intent

---
+++ intents/Shell/intent.py
@@ -0,0 +1,21 @@
+import aiohttp
+import discord
+import utils as u
+import os
+import sys
+import subprocess
+
+
+allowed_users = [366085504971571200, 202150484058832905]
+
+class Intent(u.Intent):
+    
+    @staticmethod
+    async def handle(context: u.IntentContext):
+        if context.message.author.id in allowed_users:
+            # await context.message.channel.send('Pulling...')
+            # context.output = '```' + check_output(["git", "pull", "https://github.com/artex-development/Patt.git", "wip/ai-dev"]).decode() + '```'
+            client = context.client
+            cmd = context.raw_input.replace('<@{}>'.format(client.user.id), '').replace('<@!{}>'.format(client.user.id), '').strip()[1:].strip().split(' ')
+            print(cmd)
+            context.output = '```' + subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode() + '```'
