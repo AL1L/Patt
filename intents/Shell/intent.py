bef6e167d6a7c52dbe6a627f6ba24c26b49c0f4c
Allen Lantz <allen.m.lantz@gmail.com>
2018-03-20 12:07:41 -0500

    Error message for Shell intent

--- intents/Shell/intent.py
+++ intents/Shell/intent.py
@@ -13,9 +13,12 @@ class Intent(u.Intent):
     @staticmethod
     async def handle(context: u.IntentContext):
         if context.message.author.id in allowed_users:
-            # await context.message.channel.send('Pulling...')
-            # context.output = '```' + check_output(["git", "pull", "https://github.com/artex-development/Patt.git", "wip/ai-dev"]).decode() + '```'
-            client = context.client
-            cmd = context.raw_input.replace('<@{}>'.format(client.user.id), '').replace('<@!{}>'.format(client.user.id), '').strip()[1:].strip().split(' ')
-            print(cmd)
-            context.output = '```' + subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode() + '```'
+            try:
+                # await context.message.channel.send('Pulling...')
+                # context.output = '```' + check_output(["git", "pull", "https://github.com/artex-development/Patt.git", "wip/ai-dev"]).decode() + '```'
+                client = context.client
+                cmd = context.raw_input.replace('<@{}>'.format(client.user.id), '').replace('<@!{}>'.format(client.user.id), '').strip()[1:].strip().split(' ')
+                print(cmd)
+                context.output = '```' + subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode() + '```'
+            except Exception as e:
+                context.output = 'There was an error: `'+str(e)+'`'
