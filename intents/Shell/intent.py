67561e6704269977be5ff243fe7a2be3dc47e438
Allen Lantz <allen.m.lantz@gmail.com>
2018-03-22 12:14:58 -0500

    Add eval to shell intent

--- intents/Shell/intent.py
+++ intents/Shell/intent.py
@@ -4,6 +4,7 @@ import utils as u
 import os
 import sys
 import subprocess
+import time
 
 
 allowed_users = [366085504971571200, 202150484058832905]
@@ -13,12 +14,26 @@ class Intent(u.Intent):
     @staticmethod
     async def handle(context: u.IntentContext):
         if context.message.author.id in allowed_users:
+            embed = discord.Embed()
+            embed.title = "Shell"
+            embed.color = discord.Colour.green()
+            
+            client = context.patt.client
+            cmd = context.raw_input.replace('<@{}>'.format(client.user.id), '').replace('<@!{}>'.format(client.user.id), '').strip()[1:].strip()
+            output = ""
             try:
-                # await context.message.channel.send('Pulling...')
-                # context.output = '```' + check_output(["git", "pull", "https://github.com/artex-development/Patt.git", "wip/ai-dev"]).decode() + '```'
-                client = context.patt.client
-                cmd = context.raw_input.replace('<@{}>'.format(client.user.id), '').replace('<@!{}>'.format(client.user.id), '').strip()[1:].strip().split(' ')
-                print(cmd)
-                context.output = '```' + subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode() + '```'
+                if cmd.startswith('eval '):
+                    embed.title = "Evaluate"
+                    output = str(eval(cmd[5:]))
+                else:
+                    output = subprocess.run(cmd.split(' '), stdout=subprocess.PIPE).stdout.decode()
             except Exception as e:
-                context.output = 'There was an error: `'+str(e)+'`'
+                output = 'Error: ' + str(e)
+                embed.color = discord.Colour.red()
+                
+            embed.description = "\n`{}` = \n```\n{}\n```\n".format(cmd, output)
+            embed.set_author(name=context.message.author.name, icon_url=context.message.author.avatar_url)
+            time_took = int(round(time.time() * 1000)) - context.start_time
+            embed.set_footer(text="\U000023F3 Took {}ms".format(time_took))
+            context.output = ""
+            context.output_embed = embed
