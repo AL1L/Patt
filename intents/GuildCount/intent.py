611cd4621471466ef0b77673a1a30a9659bb5daf
Allen Lantz <allen.m.lantz@gmail.com>
2018-03-24 16:26:24 -0500

    This is why we don't test in prod

--- intents/GuildCount/intent.py
+++ intents/GuildCount/intent.py
@@ -6,4 +6,4 @@ class Intent(u.Intent):
     
     @staticmethod
     async def handle(context: u.IntentContext):
-        context.output = context.output.replace('%guilds%', len(context.patt.client.guilds))
+        context.output = context.output.replace('%guilds%', str(len(context.patt.client.guilds)))
