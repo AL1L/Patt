d3b96724dbc8fe2e0010191d298fbe3638d3647f
Allen Lantz <allen.m.lantz@gmail.com>
2018-03-24 16:03:33 -0500

    Clear SetAge intent

--- intents/SetAge/intent.py
+++ intents/SetAge/intent.py
@@ -6,7 +6,4 @@ class Intent(u.Intent):
     
     @staticmethod
     async def handle(context: u.IntentContext):
-        context.patt.cursor.execute("SELECT * FROM guilds")
-        rows = context.patt.cursor.fetchall()
-        gamt = len(rows)
-        context.output = context.output.replace('%guilds%', str(gamt))
+        pass
